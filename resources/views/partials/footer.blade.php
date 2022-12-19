e7a67f2cb5925175c2664e1340c2199a96b8124a
karfaisarra <karfaisarra@gmail.com>
2022-12-19 15:56:45 +0100

    add partials header & footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,30 @@
 <footer class="bg-dark text-white py-5">
     <div class="container">
         <div class="row">
-            <div class="col text-center">Footer</div>
+            <div class="col">
+                <h3>Links</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>Contacts</li>
+                    <li>News</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Contact Us</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>Contacts</li>
+                    <li>News</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Learn more</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>Contacts</li>
+                    <li>News</li>
+                </ul>
+            </div>
         </div>
     </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,10 +11,10 @@
                         <a class="nav-link {{Route::currentRouteName() === 'home'? 'active' : ''}}" href="{{route('home')}}" aria-current="page">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{Route::currentRouteName() === 'contacts'? 'active' : ''}}" href="#">Contacts</a>
+                        <a class="nav-link {{Route::currentRouteName() === 'contacts'? 'active' : ''}}" href="{{route('contacts')}}">Contacts</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{Route::currentRouteName() === 'news'? 'active' : ''}}" href="#">News</a>
+                        <a class="nav-link {{Route::currentRouteName() === 'news'? 'active' : ''}}" href="{{route('news')}}">News</a>
                     </li>
                 </ul>
             </div>
